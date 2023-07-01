05752c0b328049bce63b24b3b0ce105755be1631
BuildTools <unconfigured@null.spigotmc.org>
2023-07-01 09:54:00 +0200

    Upload 02Upload 01

---
+++ BedrockManager/MCManagerBedrock/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MCManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtOut = new TextBox();
+            txtIn = new TextBox();
+            btnstart = new Button();
+            btnstop = new Button();
+            cPlayers = new ComboBox();
+            SuspendLayout();
+            // 
+            // txtOut
+            // 
+            txtOut.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtOut.Location = new Point(12, 12);
+            txtOut.Multiline = true;
+            txtOut.Name = "txtOut";
+            txtOut.ReadOnly = true;
+            txtOut.ScrollBars = ScrollBars.Vertical;
+            txtOut.Size = new Size(776, 327);
+            txtOut.TabIndex = 99;
+            txtOut.TabStop = false;
+            // 
+            // txtIn
+            // 
+            txtIn.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtIn.AutoCompleteCustomSource.AddRange(new string[] { "gamemode", "time set day", "gamerule" });
+            txtIn.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            txtIn.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            txtIn.Location = new Point(12, 415);
+            txtIn.Name = "txtIn";
+            txtIn.Size = new Size(584, 23);
+            txtIn.TabIndex = 1;
+            txtIn.KeyUp += txtIn_KeyUp;
+            // 
+            // btnstart
+            // 
+            btnstart.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnstart.Location = new Point(12, 345);
+            btnstart.Name = "btnstart";
+            btnstart.Size = new Size(132, 64);
+            btnstart.TabIndex = 2;
+            btnstart.TabStop = false;
+            btnstart.Text = "Start";
+            btnstart.UseVisualStyleBackColor = true;
+            btnstart.Click += btnstart_Click;
+            // 
+            // btnstop
+            // 
+            btnstop.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnstop.Location = new Point(150, 345);
+            btnstop.Name = "btnstop";
+            btnstop.Size = new Size(132, 64);
+            btnstop.TabIndex = 3;
+            btnstop.TabStop = false;
+            btnstop.Text = "Stop";
+            btnstop.UseVisualStyleBackColor = true;
+            btnstop.Click += btnstop_Click;
+            // 
+            // cPlayers
+            // 
+            cPlayers.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            cPlayers.FormattingEnabled = true;
+            cPlayers.Location = new Point(593, 415);
+            cPlayers.Name = "cPlayers";
+            cPlayers.Size = new Size(195, 23);
+            cPlayers.Sorted = true;
+            cPlayers.TabIndex = 4;
+            cPlayers.TabStop = false;
+            cPlayers.TextChanged += cPlayers_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cPlayers);
+            Controls.Add(btnstop);
+            Controls.Add(btnstart);
+            Controls.Add(txtIn);
+            Controls.Add(txtOut);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtOut;
+        private TextBox txtIn;
+        private Button btnstart;
+        private Button btnstop;
+        private ComboBox cPlayers;
+    }
+}
---
+++ BedrockManager/MCManagerBedrock/Form1.cs
@@ -0,0 +1,176 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MCManager
+{
+    public partial class Form1 : Form
+    {
+        Process p = new Process();
+        ProcessStartInfo pi = new ProcessStartInfo();
+        List<string> Output = new List<string>();
+        List<string> players = new List<string>();
+        Task task;
+        StreamWriter sw;
+        bool ServerRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Action Start()
+        {
+            Dictionary<string, string> set = new Dictionary<string, string>();
+            using (StreamReader sr = new StreamReader("settings.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains('='))
+                    {
+                        string[] tmp = line.Split('=');
+                        set.Add(tmp[0], tmp[1]);
+                    }
+                }
+            }
+
+            //string path = "C:\\Users\\Fakes\\Desktop\\Test Server Bedrock\\bedrock_server.exe";
+
+            //pi.Arguments = path;
+            pi.FileName = Directory.GetCurrentDirectory() + set["path"];
+            pi.UseShellExecute = false;
+            pi.RedirectStandardOutput = true;
+            pi.RedirectStandardInput = true;
+            pi.CreateNoWindow = true;
+            p.StartInfo = pi;
+            p.OutputDataReceived += new DataReceivedEventHandler(pOut);
+            p.Start();
+            sw = p.StandardInput;
+            p.BeginOutputReadLine();
+            //Invoke((MethodInvoker)delegate { Refresh(); });
+            //while (true)
+            //{
+            //    p.BeginOutputReadLine();
+            //    p.CancelOutputRead();
+            //    Thread.Sleep(1000);
+            //}
+            //p.CancelOutputRead();
+            ServerRunning = true;
+            return null;
+        }
+
+        private void pOut(object sender, DataReceivedEventArgs e)
+        {
+            Output.Add(e.Data);
+            Invoke((MethodInvoker)delegate { txtOut.Text += e.Data + Environment.NewLine; });
+            if (ServerRunning && e.Data != null)
+            {
+                if (e.Data.Contains("Player connected"))
+                {
+                    string[] tmp = e.Data.Split(':');
+                    for (int i = 0; i < tmp.Length; i++)
+                    {
+                        if (tmp[i].Contains("Player connected"))
+                        {
+                            string[] tmpname = tmp[i + 1].Split(',');
+                            if (!players.Contains(tmpname[0]))
+                            {
+                                players.Add(tmpname[0].Trim());
+                                Invoke((MethodInvoker)delegate { cPlayers.Items.Add('"' + tmpname[0].Trim() + '"'); });
+                                AutoCompleteStringCollection names = txtIn.AutoCompleteCustomSource;
+                                Invoke((MethodInvoker)delegate { names.Add('"' + tmpname[0].Trim() + '"'); });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtIn_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    p.CancelOutputRead();
+                }
+                finally
+                {
+                    if (txtIn.Text == "gm1")
+                    {
+                        sw.WriteLine("gamemode 1 " + '"' + "Basti F6648" + '"');
+                    }
+                    else if (txtIn.Text == "gm0")
+                    {
+                        sw.WriteLine("gamemode 0 " + '"' + "Basti F6648" + '"');
+                    }
+                    else
+                    {
+                        sw.WriteLine(txtIn.Text);
+                    }
+                    p.BeginOutputReadLine();
+                    txtIn.Text = "";
+                    cPlayers.Text = "";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            task = Task.Factory.StartNew(() => Start());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ServerRunning) btnstop.PerformClick();
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                p.CancelOutputRead();
+            }
+            finally
+            {
+                sw.WriteLine("stop");
+                p.BeginOutputReadLine();
+                p.CancelOutputRead();
+                Thread.Sleep(1000);
+                p.Close();
+                StreamWriter log = new StreamWriter("log.txt");
+                log.Write(txtOut.Text);
+                log.Close();
+                txtOut.Text = "Server stopped";
+                ServerRunning = false;
+            }
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            if (!ServerRunning)
+            {
+                txtOut.Text = "";
+                p.Start();
+                p.BeginOutputReadLine();
+                ServerRunning = true;
+            }
+        }
+
+        private void cPlayers_TextChanged(object sender, EventArgs e)
+        {
+            txtIn.Text += cPlayers.Text;
+            cPlayers.Text = "";
+            txtIn.Select();
+            //try
+            //{
+            //    p.CancelOutputRead();
+            //}
+            //finally
+            //{
+            //    sw.WriteLine(txtIn.Text);
+            //    p.BeginOutputReadLine();
+            //}
+        }
+    }
+}
